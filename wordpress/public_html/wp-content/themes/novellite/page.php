4bd1d4daa52664f32cd8d224f2191d41fcca4811
tyler.gary <tyler.gary@appdynamics.com>
2016-01-16 14:21:11 -0800

    Remove Page sidebar

--- wordpress/public_html/wp-content/themes/novellite/page.php
+++ wordpress/public_html/wp-content/themes/novellite/page.php
@@ -15,7 +15,7 @@
     <div class="container">
         <div class="row">
             <div class="page-content">
-                <div class="col-md-9">
+                <div class="col-md-12">
                     <div class="content-bar gallery"> 
                         <h1 class="featured_title"><?php the_title(); ?></h1>
             <?php while (have_posts()) : the_post(); ?>
@@ -28,11 +28,6 @@
 						</div>
                     </div>
                 </div>
-				<div class="col-md-3">
-		<!--Start Sidebar-->
-		<?php get_sidebar(); ?>
-		<!--End Sidebar-->
-		</div> 
             </div>
         </div>
         <div class="clear"></div>
