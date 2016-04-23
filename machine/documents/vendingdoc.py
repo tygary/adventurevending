4a95d7660829de0c1dc4b43714bcd8928c271bfb
trjohnst <tpickles211@gmail.com>
2016-04-23 12:29:41 -0700

    Initial commit for PyGui app

---
+++ machine/documents/vendingdoc.py
@@ -0,0 +1,38 @@
+import pickle
+from GUI import Document
+
+
+class VendingDoc(Document):
+
+    blobs = None
+
+    def new_contents(self):
+        self.blobs = []
+
+    def read_contents(self, file):
+        self.blobs = pickle.load(file)
+
+    def write_contents(self, file):
+        pickle.dump(self.blobs, file)
+
+    def add_blob(self, blob):
+        self.blobs.append(blob)
+        self.changed()
+        self.notify_views('blob_changed', blob)
+
+    def find_blob(self, x, y):
+        for blob in self.blobs:
+            if blob.contains(x, y):
+                return blob
+        return None
+
+    def move_blob(self, blob, dx, dy):
+        self.notify_views('blob_changed', blob)
+        blob.move(dx, dy)
+        self.changed()
+        self.notify_views('blob_changed', blob)
+
+    def delete_blob(self, blob):
+        self.notify_views('blob_changed', blob)
+        self.blobs.remove(blob)
+        self.changed()
---
+++ machine/machineui.py
@@ -0,0 +1,48 @@
+#
+#  BlobEdit - A totally silly application for showing
+#  ========   off the GUI framework and providing an example
+#             of its use.
+#
+#  Blobs are red squares that you place on a Blob Document
+#  by clicking and move around by dragging. You can save
+#  your blob arrangement in a file and load it back later
+#  to impress your friends (or send them away screaming).
+#
+#  News flash: Got a blob you don't want? Now you can
+#  get rid of it by shift-clicking it! Isn't that useful!
+#
+
+from GUI import Application, Window, Cursor
+from GUI.Files import FileType
+from views.vendingview import VendingView
+from documents.vendingdoc import VendingDoc
+
+
+class VendingApp(Application):
+
+    def __init__(self):
+        Application.__init__(self)
+        self.vending_type = FileType(name = "Adventure Vending Document", suffix = "vending",
+                                  #mac_creator = "BLBE", mac_type = "BLOB", # These are optional
+                                  )
+        self.file_type = self.vending_type
+        self.vending_cursor = Cursor("cursors/arrow.tiff")
+
+    def open_app(self):
+        self.new_cmd()
+
+    def make_document(self, fileref):
+        return VendingDoc(file_type = self.vending_type)
+
+    def make_window(self, document):
+        win = Window(size = (400, 400), document = document)
+        view = VendingView(model = document, extent = (1000, 1000), scrolling = 'hv',
+                        cursor = self.vending_cursor)
+        win.place(view, left = 0, top = 0, right = 0, bottom = 0, sticky = 'nsew')
+        win.show()
+
+
+
+
+
+VendingApp().run()
