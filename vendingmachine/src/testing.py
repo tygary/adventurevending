9c50d7f463d7bdbf4a07dd3c8a34fcc8cda5656f
trjohnst <tpickles211@gmail.com>
2016-06-18 17:54:39 -0700

    Moving todos to readme

--- vendingmachine/src/testing.py
+++ vendingmachine/src/testing.py
@@ -160,22 +160,3 @@ class VendingMachine(object):
 
     def stop(self):
         self.server.stop()
-
-
-
-
-
-
-
-
-"""
-  TODO
-Support Using the Have a way to input an adventure and control which adventures you get
- - We need a Enable/Disable certain adventure  (scheduled)
- - Each adventure can only be dispensed a certain number of times
-
-Play Sounds
-
-Maintenance
- - Unlock all the boxes (possibly 9 at a time)
-"""
