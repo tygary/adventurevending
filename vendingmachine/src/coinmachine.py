d4a53071e3fee65ae4179cdad8d1121eafabecac
trjohnst <tpickles211@gmail.com>
2016-06-30 18:56:12 -0700

    Adding logging to the coinmachine.py

--- vendingmachine/src/coinmachine.py
+++ vendingmachine/src/coinmachine.py
@@ -38,32 +38,35 @@ class CoinMachine(object):
     # Public --------------------------------------------
 
     def start_waiting_for_coin(self):
-        self.logger.log("waiting for coin at pin %s" % self.coin_input_pin)
+        self.logger.log("Coin: waiting for coin at pin %s" % self.coin_input_pin)
         add_event_detection(self.coin_input_pin, callback=self.__coin_cb)
         add_event_detection(self.coin_counter_input_pin, callback=self.__coin_counter_cb)
         self.waiting_for_coin = True
 
     def clear_coins(self):
+        self.logger.log("Coin: clearing count")
         self.__set_coin_count(0)
 
     def subtract_coins(self, num):
+        self.logger.log("Coin: new coin count = %s - %s" % (self.current_value, num))
         self.__set_coin_count(self.current_value - num)
 
     # Private -------------------------------------------
 
     def __coin_cb(self, channel):
+        self.logger.log("Coin: coin cb with waiting status: %s" % self.waiting_for_coin)
         if self.waiting_for_coin == True:
-            self.logger.log("coin slot triggered")
             self.__coin_detected()
             self.coin_detected = True
 
     def __coin_counter_cb(self, channel):
+        self.logger.log("Coin: coin counter cb with waiting status: %s" % self.waiting_for_coin)
         if self.waiting_for_coin == True:
-            print "Coin Counter triggered"
             self.__coin_detected()
             self.coin_counted = True
 
     def __coin_detected(self):
+        self.logger.log("Coin: coin detected with pending status: %s" % self.coin_pending)
         if (self.coin_pending == False):
             self.coin_pending = True
             self.coin_detected = False
@@ -72,22 +75,25 @@ class CoinMachine(object):
             t.start()
 
     def __done_waiting_for_coin(self):
+        self.logger.log("Coin: done waiting with pending status")
         self.coin_detected = True
         #Fake the coin detection for now, it's broken
         if self.coin_detected == True and self.coin_counted == True:
-            self.logger.log("Got a coin, Pick a box")
+            self.logger.log("  Got a coin, pick a box")
             self.__set_accepted_coin(True)
         else:
-            self.logger.log("Not accepted")
+            self.logger.log("  Not accepted")
         self.coin_detected = False
         self.coin_counted = False
         self.coin_pending = False
 
     def __wait_for_coin(self):
+        self.logger.log("Coin: waiting_for_coin set to true, was: %s" % self.waiting_for_coin)
         self.waiting_for_coin = True
 
 
     def __set_accepted_coin(self, value):
+        self.logger.log("Coin: accepting coin with value %s" % value)
         self.accepted_a_coin = value;
         if value == True:
             self.lighting.coin_received()
@@ -98,14 +104,18 @@ class CoinMachine(object):
                 else:
                     self.__set_coin_count(self.current_value + 1)
             except RuntimeError:
-                self.logger.log("Setting coin status")
+                self.logger.log("  error set_accepted_coin")
 
     def __set_coin_count(self, count):
+        self.logger.log("Coin: attempting to set count to %s from %s" % (count,self.current_value))
         #Don't allow more than three credits
         if count < 0:
+            self.logger.log("  count below min, setting to min")
             count = 0
         if count > 3:
+            self.logger.log("  count exceeds max, setting to max")
             count = 3
+        self.logger.log("  setting count to %s" % count)
         self.current_value = count
         if count == 3:
             GPIO.output(self.coin_counter_pins[0], True)
