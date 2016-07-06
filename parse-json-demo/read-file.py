7895726a716a758d7bc6d7d0b4e21da675624c75
trjohnst <tpickles211@gmail.com>
2016-07-05 22:56:45 -0700

    Removing json parsing demo

--- parse-json-demo/read-file.py
+++
@@ -1,9 +0,0 @@
-import json
-from adventure import Adventure
-
-with open('file.json') as data_file:
-  data = json.load(data_file)
-
-for adventureConfig in data["adventures"]:
-  print adventureConfig
-  adventure = Adventure(adventureConfig)
