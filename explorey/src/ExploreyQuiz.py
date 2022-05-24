d1553881cd208f2d1641a97a1c32ee9fca8ef2cd
Tyler <tygary@gmail.com>
2022-05-23 18:57:14 -0700

    Fixing image path

--- explorey/src/ExploreyQuiz.py
+++ explorey/src/ExploreyQuiz.py
@@ -7,7 +7,7 @@ from fpdf import FPDF
 ##-----------------------------------------------------------------------
 class ExploreyQuiz(FPDF):
     def header(self):
-        self.image("/home/pi/adventurevending/explorey/printing/etLogo.jpg", 38, 0, 30, 30)
+        self.image("/home/admin/adventurevending/explorey/printing/etLogo.jpg", 38, 0, 30, 30)
         self.ln(30)
     def footer(self):
         self.set_y(-15)
