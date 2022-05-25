1ab1711ee0c726d70763a50e9c1acb8dcf1cbe1b
Tyler <tygary@gmail.com>
2022-05-24 21:51:38 -0700

    Trying rising edge

--- explorey/src/addeventdetection.py
+++ explorey/src/addeventdetection.py
@@ -4,14 +4,14 @@ def add_event_detection(pin, callback, bothdirections=False):
     try:
         GPIO.setup(pin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
         GPIO.remove_event_detect(pin)
-        GPIO.add_event_detect(pin, GPIO.FALLING, callback=callback)
+        GPIO.add_event_detect(pin, GPIO.RISING, callback=callback)
         if bothdirections:
-            GPIO.add_event_detect(pin, GPIO.RISING, callback=callback)
+            GPIO.add_event_detect(pin, GPIO.FALLING, callback=callback)
     except RuntimeError:
         try:
             GPIO.remove_event_detect(pin)
-            GPIO.add_event_detect(pin, GPIO.FALLING, callback=callback)
+            GPIO.add_event_detect(pin, GPIO.RISING, callback=callback)
             if bothdirections:
-                GPIO.add_event_detect(pin, GPIO.RISING, callback=callback)
+                GPIO.add_event_detect(pin, GPIO.FALLING, callback=callback)
         except RuntimeError:
             pass
